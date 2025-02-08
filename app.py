6afd4cdfe870c694e979ed74508c0921a1b458e8
reyaan-07 <reyaanjr11@gmail.com>
2025-02-08 23:30:02 +0530

    Host and port change

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def download_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from app import app  # Change 'app' to your actual module name
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000, debug= True)
